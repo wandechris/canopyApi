b879474ec7d516a68ed298fa020d05bcc54cd5c0
Akinwande Adegbola <akinwande.adegbola@interswitchgroup.com>
2017-01-03 08:07:24 +0100

    url

--- app/Http/Controllers/Events.php
+++ app/Http/Controllers/Events.php
@@ -45,7 +45,7 @@ class Events extends Controller
             $photos = Photos::where('eventId', $event->id)->get();
             foreach($photos as $photo) {
                 $contents = Storage::disk('s3')->get($event->id.$photo->name.'.png');
-                $photo->value = base64_encode($contents);
+                $photo->value =$contents;
             }
             $event->photos = $photos;
         }
@@ -152,8 +152,8 @@ class Events extends Controller
         {
             $photos = Photos::where('eventId', $id)->get();
             foreach($photos as $photo) {
-                $contents = Storage::disk('s3')->get($id.$photo->name.'.png');
-                $photo->value = base64_encode($contents);
+                $contents = Storage::url('s3')->url($id.$photo->name.'.png');
+                $photo->value =$contents;
             }
             $event->photos = $photos;
             return $event;
